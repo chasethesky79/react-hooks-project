ff7406267da793db03fa6c965cda893964acf21a
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2020-12-06 20:35:32 -0600

    react-hooks-project
    Added initial implementation for Register component

--- src/user/Register.js
+++ src/user/Register.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Register({setUser}) {
-    const handleOnRegister = (evt) => {
-        setUser('Daniel Blog');
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+
+    const handleUserNameChange = ({target: { value }}) => {
+        setUserName(value);
     }
-    
+
+    const handlePasswordChange = ({target: { value }}) => {
+        setPassword(value);
+    }
+
+    const handleRepeatPasswordChange = ({target: { value }}) => {
+        setRepeatPassword(value);
+    }
+
+    const handleFormSubmit = (evt) => {
+        if (password !== repeatPassword) {
+            return;
+        }
+        setUser(userName);
+        evt.preventDefault();
+    }
+
     return (
-        <form onSubmit={e => e.preventDefault()}>
+        <form onSubmit={handleFormSubmit}>
             <label htmlFor="register-username">Username:</label>
-            <input type="text" name="register-username" id="register-username"/>
+            <input type="text" name="register-username" value ={userName} id="register-username" onChange={handleUserNameChange}/>
 
             <label htmlFor="register-password">Password:</label>
-            <input type="text" name="register-password" id="register-password"/>
+            <input type="text" name="register-password" value={password} id="register-password" onChange={handlePasswordChange}/>
 
             <label htmlFor="register-repeat-password">Repeat Password:</label>
-            <input type="text" name="register-repeat-password" id="register-repeat-password"/>
+            <input type="text" name="register-repeat-password" value={repeatPassword} id="register-repeat-password" onChange={handleRepeatPasswordChange}/>
 
-            <input type="submit" value="Register" onClick={handleOnRegister}/>
+            <input type="submit" value="Register" disabled={!userName}/>
         </form>
     )
 }
