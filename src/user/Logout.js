0bb75b4f5b675b131fa515302f2cfeb6aa5e7b31
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2020-12-06 20:45:48 -0600

    react-hooks-project
    Implemented functionality for Logout button

--- src/user/Logout.js
+++ src/user/Logout.js
@@ -1,8 +1,13 @@
-export default function Logout({user}){
+export default function Logout({ user, setUser }){
+
+  const handleLogout = (evt) => {
+      setUser('')
+      evt.preventDefault();
+  }
   return (
       <form onSubmit={e => e.preventDefault}>
        Logged in as: <b>{user}</b>
-       <input type="submit" value="Logout"/>
+       <input type="submit" value="Logout" onClick={handleLogout}/>
       </form>
   )
 }
